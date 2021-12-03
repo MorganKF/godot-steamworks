10c0f4d9cfc768733f013d0b8dfc7166dc94a0b3
MorganKF <mfudge92@gmail.com>
2021-12-03 19:36:59 -0330

    Add header guards

--- src/register_types.hpp
+++ src/register_types.hpp
@@ -1,3 +1,7 @@
-#pragma once
+#ifndef GODOT_STEAMWORKS_REGISTER_TYPES_H
+#define GODOT_STEAMWORKS_REGISTER_TYPES_H
+
 void register_godot_steamworks_types();
 void unregister_godot_steamworks_types();
+
+#endif // GODOT_STEAMWORKS_REGISTER_TYPES_H
--- src/steamworks.hpp
+++ src/steamworks.hpp
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef GODOT_STEAMWORKS_STEAMWORKS_H
+#define GODOT_STEAMWORKS_STEAMWORKS_H
+
 #include <godot_cpp/classes/ref_counted.hpp>
 #include <godot_cpp/core/binder_common.hpp>
 
@@ -16,3 +18,5 @@ public:
     Steamworks();
     ~Steamworks() override;
 };
+
+#endif // GODOT_STEAMWORKS_STEAMWORKS_H
