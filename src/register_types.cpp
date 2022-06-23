2766326b132645891ccf36f772812c3ab6d42d95
MorganKF <mfudge92@gmail.com>
2022-06-23 20:40:28 -0230

    Fix compilation for alpha 10

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -12,7 +12,10 @@ using namespace godot;
 
 Ref<Steamworks> steamworks_ref;
 
-void register_godot_steamworks_types() {
+void register_godot_steamworks_types(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
 	ClassDB::register_class<Steamworks>();
 	ClassDB::register_class<SteamPacketPeer>();
 	ClassDB::register_class<SteamMultiplayerPeer>();
@@ -24,7 +27,10 @@ void register_godot_steamworks_types() {
 	engine->register_singleton("Steamworks", steamworks_ref.ptr());
 }
 
-void unregister_godot_steamworks_types() {
+void unregister_godot_steamworks_types(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
 	if (steamworks_ref.is_valid()) {
 		steamworks_ref.unref();
 	}
@@ -32,7 +38,8 @@ void unregister_godot_steamworks_types() {
 
 extern "C" GDNativeBool GDN_EXPORT godot_steamworks_init(const GDNativeInterface *p_interface, GDNativeExtensionClassLibraryPtr p_library, GDNativeInitialization *r_initialization) {
     GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
-	init_obj.register_scene_initializer(register_godot_steamworks_types);
-	init_obj.register_scene_terminator(unregister_godot_steamworks_types);
+	init_obj.register_initializer(register_godot_steamworks_types);
+	init_obj.register_terminator(unregister_godot_steamworks_types);
+    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 	return init_obj.init();
 }
